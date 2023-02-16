f7e8f933145fb2b2f4a69e4529545d686cc00123
Sun Sreng <sun_sreng@hotmail.com>
2023-02-16 22:20:22 +0700

    ok

--- apps/gmana_wallet/lib/core/utils/g_const.dart
+++ apps/gmana_wallet/lib/core/utils/g_const.dart
@@ -3,7 +3,5 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 final supabase = Supabase.instance.client;
 const additionalRedirectURLs = 'co.gmana.gmanawalletapp://login-callback/';
-// const String url = 'https://lwfkzthkngsyclkdbdpc.supabase.co';
-// const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTc1MzMzOCwiZXhwIjoxOTUxMzI5MzM4fQ.sMqkSsTDYTDz79hxGETUWRxb1y_5-8JMF-EdIQtC-GA';
 
 const sizedBoxHeight = SizedBox(height: 10);
